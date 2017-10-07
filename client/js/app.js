05e80f9ff46c43b00085247acab467fadb01e3ca
Utkarsh Sengar <utkarsh2012@gmail.com>
2017-10-06 23:16:38 -0700

    Removed cors-anywhere

--- client/js/app.js
+++ client/js/app.js
@@ -5,24 +5,6 @@
 \*------------------------------------*/
 
 
-(function() {
-    var cors_api_host = 'cors-anywhere.herokuapp.com';
-    var cors_api_url = 'https://' + cors_api_host + '/';
-    var slice = [].slice;
-    var origin = window.location.protocol + '//' + window.location.host;
-    var open = XMLHttpRequest.prototype.open;
-    XMLHttpRequest.prototype.open = function() {
-        var args = slice.call(arguments);
-        var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
-        if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
-            targetOrigin[1] !== cors_api_host) {
-            args[1] = cors_api_url + args[1];
-        }
-        return open.apply(this, args);
-    };
-})();
-
-
 twopence = angular.module('twopence', [
      'ui.router',
      'vesparny.fancyModal'
@@ -362,7 +344,7 @@ twopence.config(
 
 
 
-}]).constant("BASE_URL", "https://api.twopence.co");
+}]).constant("BASE_URL", "http://localhost:8000/api");
 
 twopence.run(
     ['$rootScope',
