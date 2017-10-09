3873fce0448c4cc02ce5163b1cda124fc03a5a9b
Guillermo Casanova <hi.guillermocasanova@gmail.com>
2017-10-09 16:48:12 -0400

    -Finishing setup for sign up to succesfully create a user account and then a sponsor account via the "identity" step of the sign up process
     - Creating a setter function to set a auth token when someone logs in or creates a sponsor account
    
     - Tesying sponsor account creation from start to finish and from the "identity" step incase a sponsor is logged in but somehow skipped the full sign up process

--- client/js/home/signUpCtrl.js
+++ client/js/home/signUpCtrl.js
@@ -6,15 +6,21 @@
 \*------------------------------------*/
 
 twopence.controller('signUpCtrl', [
+    '$filter', 
     '$scope',
     '$state',
     '$timeout',
     'User',
+    'Sponsor',
+    'Auth', 
     function(
+      $filter, 
       $scope,
       $state,
       $timeout, 
-      User) {
+      User, 
+      Sponsor,
+      Auth) {
 
       var vm = this;
 
@@ -24,10 +30,40 @@ twopence.controller('signUpCtrl', [
 
       vm.userCreationForm = {}; 
 
+      vm.sposorCreationForm = {}; 
+
+
+      //
+      // We store the sponsor info and user info as objects to then
+      // inject into the requests 
+      //
+      vm.sponsorInfo = { 
+
+        'sms_preferred': false
+
+      }; 
+
 
       vm.userInfo = {
         'phone': ''
       }; 
+
+
+      //
+      // We store the DOB in a separate property because angular 1.3+ 
+      // ng-model for type='date' in input fields expects dates and not strings
+      // or converted dates 
+      vm.sponsorDob = new Date('2017', 9, 22); 
+
+
+      if(Auth.getToken()) {
+
+        $state.go('main.signUp.identity'); 
+
+      } else {
+
+        $state.go('main.signUp.account'); 
+      }
 
 
       //
@@ -42,12 +78,41 @@ twopence.controller('signUpCtrl', [
           User.create(vm.userInfo).then(function() {
 
             console.log('yep, it passes ');
+            $state.go('main.signUp.identity'); 
+
 
           }); 
 
         } else {
 
           console.log('not yet boy'); 
+
+        }
+
+      }; 
+
+
+      //
+      // Creates the sponsor fom the information provided and the logged in account 
+      //
+      vm.createSponsor = function(pSponsorForm) {
+
+        if(pSponsorForm.$valid) {
+
+          vm.sponsorInfo.dob = $filter('date')(vm.sponsorDob, 'yyyy-MM-dd');
+
+          Sponsor.create(vm.sponsorInfo); 
+
+          console.log('valid'); 
+
+          $state.go('sponsor.dashboard'); 
+
+
+        } else {
+
+
+          console.log('not valid')
+
 
         }
 
