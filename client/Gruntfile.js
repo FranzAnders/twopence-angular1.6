09323d71a40a944cb96b13afb17708374c4ba96c
Guillermo Casanova <hi.guillermocasanova@gmail.com>
2017-10-09 14:53:07 -0400

    - Refactoring project to use grunt-connect to run server instead of express server with node
    - Testing and verifyding use ws to spin up proxy domain and current setup properly work in making calls to onepence api

---
+++ client/Gruntfile.js
@@ -0,0 +1,174 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    require('load-grunt-tasks')(grunt);
+
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		app: '.',
+		dist: 'dist',
+
+		sass: {
+			dist: {
+				options: {
+					style: 'expanded', // expanded or nested or compact or compressed
+					loadPath: '<%= app %>/bower_components/foundation/scss',
+					compass: true,
+					quiet: false,
+          update: true,
+          lineNumbers: true
+				},
+				files: {
+					'<%= app %>/css/app.css': '<%= app %>/scss/main.scss'
+				}
+			}
+		},
+
+
+
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all: [
+				'Gruntfile.js',
+				'<%= app %>/js/**/*.js'
+			]
+		},
+
+		clean: {
+			dist: {
+				src: ['<%= dist %>/*']
+			},
+		},
+		copy: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd:'<%= app %>/',
+					src: ['assets/fonts/**', '**.png', '**.ico', '**/*.html','*.xml', '**/*.php', '!**/*.scss', '!bower_components/**', '!node_modules/**', 'data/**'],
+					dest: '<%= dist %>/'
+				} ]
+			},
+		},
+
+		imagemin: {
+			target: {
+				files: [{
+					expand: true,
+					cwd: 'assets/images/',
+					src: ['**/*.{jpg,gif,svg,jpeg,png}'],
+					dest: '<%= dist %>/assets/images/'
+				}]
+			}
+		},
+
+		uglify: {
+			options: {
+				preserveComments: 'some',
+				mangle: false
+			}
+		},
+
+		useminPrepare: {
+			html: ['<%= app %>/index.html'],
+			options: {
+				dest: '<%= dist %>'
+			}
+		},
+
+		usemin: {
+			html: ['<%= dist %>/**/*.html', '!<%= app %>/bower_components/**'],
+			css: ['<%= dist %>/css/**/*.css'],
+			options: {
+				dirs: ['<%= dist %>']
+			}
+		},
+
+		watch: {
+			grunt: {
+				files: ['Gruntfile.js'],
+				tasks: ['sass']
+			},
+			sass: {
+				files: '<%= app %>/scss/**/*.scss',
+				tasks: ['sass']
+			},
+			livereload: {
+				files: ['<%= app %>/**/*.html', '<%= app %>/data/*.json', '!<%= app %>/bower_components/**', '<%= app %>/js/**/*.js', '<%= app %>/css/**/*.css', '<%= app %>/images/**/*.{jpg,gif,svg,jpeg,png}'],
+				options: {
+					livereload: 35729
+				}
+			}
+		},
+
+		connect: {
+			app: {
+				options: {
+					port: 8080,
+					base: '<%= app %>/',
+					livereload: 35729
+            }
+		},
+			dist: {
+				options: {
+					port: 8080,
+					base: '<%= dist %>/',
+					keepalive: true,
+					livereload: false,
+				}
+			}
+		},
+
+		wiredep: {
+			target: {
+				src: [
+					'<%= app %>/**/*.html'
+				],
+				exclude: [
+					'modernizr',
+					'/foundation'
+				]
+			},
+      options: {
+          bowerJson: require('./bower.json')
+      }
+		},
+      svgstore: {
+        options: {
+          cleanup: true,
+          svg: {
+            style: "display: none;"
+          }
+        },
+        default: {
+            files: {
+                '<%= app %>/images/ingredients/ingredients.svg' : ['<%= app %>/images/icons/*.svg'],
+            }
+        }
+      },
+      'json-minify': {
+          build: {
+            files: '<%= dist %>/data/*.json'
+          }
+        }
+	});
+
+
+    grunt.loadNpmTasks('grunt-svgstore');
+    grunt.loadNpmTasks('grunt-json-minify');
+
+    grunt.registerTask('compile-sass', ['sass']);
+    grunt.registerTask('bower-install', ['wiredep']);
+
+    grunt.registerTask('default', ['compile-sass', 'bower-install', 'connect:app', 'watch']);
+    grunt.registerTask('validate-js', ['jshint']);
+    grunt.registerTask('server-dist', ['connect:dist']);
+    grunt.registerTask('publish-noimg', ['compile-sass', 'clean:dist', 'useminPrepare', 'copy:dist', 'concat', 'cssmin', 'uglify','json-minify:build', 'usemin']);
+
+    //grunt.registerTask('publish', ['compile-sass', 'clean:dist', 'useminPrepare', 'copy:dist', 'newer:imagemin', 'concat', 'cssmin', 'uglify', 'json-minify:build','usemin']);
+    grunt.registerTask('publish', ['compile-sass', 'clean:dist', 'useminPrepare', 'copy:dist', 'concat', 'cssmin', 'uglify', 'json-minify:build','usemin']);
+
+};
