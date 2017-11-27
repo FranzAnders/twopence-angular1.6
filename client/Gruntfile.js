8272928b103352abab2c274e49dc2b1040c505fe
Guillermo Casanova <hi.guillermocasanova@gmail.com>
2017-11-27 18:03:06 -0500

    - Fixing gruntfile issue with dist not being defined

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -7,6 +7,7 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		app: '.',
+    dist: 'dist',
      compass: {
        dist: {
          options: {
