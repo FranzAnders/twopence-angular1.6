f033ef40cc98e5487befb526c9e60c5b56749845
Guillermo Casanova <hi.guillermocasanova@gmail.com>
2017-12-05 09:38:56 -0500

    - Working on styles for app navigation and main navigation
     - Creating directive for main navigaton
    
     - Adding prototype off canvas navigation concept for secondary app nav links

--- client/js/navigation/appNavDir.js
+++ client/js/navigation/appNavDir.js
@@ -13,10 +13,11 @@ twopence.directive('appNavDir', function() {
     restrict: "E", 
     scope: {}, 
     replace: true, 
-    controller: ['$state', function($state) {
+    controller: ['$scope', '$state', function($scope, $state) {
 
         var vm = this; 
 
+        vm.secondaryMenuActive = false; 
 
         vm.isActive = function(pState) {
 
@@ -27,12 +28,42 @@ twopence.directive('appNavDir', function() {
         }; 
 
 
+        //
+        // Displays the secondary menu that is of canvas 
+        //
+        vm.toggleSecondaryMenu  = function() {
+
+        vm.secondaryMenuActive = !vm.secondaryMenuActive; 
+
+          if(vm.secondaryMenuActive) {
+
+            $scope.$emit('app-nav-is-open');
+
+          } else {
+            $scope.$emit('app-nav-is-closed');
+
+          }
+
+        }; 
+
+
     }], 
     controllerAs: 'nav', 
     bindToController: true, 
     templateUrl: "js/navigation/appNav.html",
     link: function(scope, attrs, element) {
 
+      scope.$on('app-nav-is-open', function(event, data) {
+
+        $('.app-nav-wrapper').addClass('app-nav-is-open');
+        
+      });
+
+      scope.$on('app-nav-is-closed', function(event, data) {
+
+        $('.app-nav-wrapper').removeClass('app-nav-is-open');
+
+      });
 
 
     }
