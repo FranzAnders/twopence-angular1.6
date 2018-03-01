b90825478cc81e6c4e0cfadefe7e611c604bf2b5
Will Gilman <will@samadhi.tech>
2018-03-01 23:26:47 +0000

    Merged in fixing_referral_param_with_service (pull request #16)
    Fixing referral param with service
    
    *  - Capturing the referral query param if its available in mainCtrl when the page first lods
    
         - Making sign up forms get a referral name if its available when a user fills a form  via the Referrals sevice
    
    * Changed to null operator.

--- client/js/main/mainCtrl.js
+++ client/js/main/mainCtrl.js
@@ -5,14 +5,18 @@
     Main Controller
 \*------------------------------------*/
 
-twopence.controller('mainCtrl', ['$rootScope', '$scope', '$state', 'Funnel',
-    function($rootScope, $scope, $state, Funnel) {
+twopence.controller('mainCtrl', ['$rootScope', '$scope', '$state', '$location', 'Funnel', 'Referrals',
+    function($rootScope, $scope, $state, $location, Funnel, Referrals) {
 
     var vm = this;
 
     $scope.$state = $state;
 
     vm.funnelIsHidden = Funnel.getState();
+
+    if($location.search().referredby) {
+      Referrals.setReferral($location.search().referredby)
+    }
 
     $rootScope.$on('$stateChangeSuccess', function() {
 
