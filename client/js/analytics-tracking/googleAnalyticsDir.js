7b743892c4fde29c835feb2c7d480b71e0395dcd
hello@adamwagner.io <hello@adamwagner.io>
2018-03-03 22:51:03 +0000

    Merged in google-analytics-tracking (pull request #20)
    Google analytics tracking
    
    * Adding Google Analytics Token to env-config
    
    * Adding Google Analytics Tracking Directive
    
    * Updating index.html to include and utilize Google Analytics directive
    
    * Updating app.js to register pageviews for SPA. Generated local env file via grunt.
    
    * Cleaning up diff

---
+++ client/js/analytics-tracking/googleAnalyticsDir.js
@@ -0,0 +1,37 @@
+
+'use strict';
+
+/*------------------------------------*\
+    #Google Analytics Directive
+\*------------------------------------*/
+
+twopence.directive('googleAnalyticsDir',[
+    'ENV',
+    '$timeout',
+    function(ENV, $timeout) {
+
+  return {
+    restrict: 'A',
+    link:function(scope, element, attrs) {
+
+      $timeout(function() {
+
+        // dynamically add external script tag
+        var s = document.createElement('script');
+        s.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id='+ENV.googleAnalyticsToken);
+        s.async = true;
+        document.head.appendChild(s);
+
+        // init Google Analytics
+        window.dataLayer = window.dataLayer || [];
+        window.gtag = function(){dataLayer.push(arguments);}
+        window.gtag('js', new Date());
+        window.gtag('config', ENV.googleAnalyticsToken);
+
+      }, 0);
+
+    }
+
+  }
+
+}]);
